c001b8d7860001a965e393ef1cd824bdc66ee08d
Álvaro Duda <alvarodudadahoradh@gmail.com>
2024-09-24 14:01:34 -0300

    Control

---
+++ README.md
@@ -0,0 +1,4 @@
+# Descrição
+
+Projeto consistente no desenvolvimento de um sistema interno para o controle remoto de uma TV. O objetivo é criar uma interface funcional que permita o controle completo da TV, incluindo o ajuste de volume, abrir menu, e outros recursos.
+
---
+++ src/Acoes.java
@@ -0,0 +1,10 @@
+public class Acoes {
+    public static void main(String[] args) {
+        ControleRemoto controle = new ControleRemoto();
+
+        controle.maisVolume();
+        controle.play();
+        controle.abrirMenu();
+        controle.fecharMenu();
+    }
+}
